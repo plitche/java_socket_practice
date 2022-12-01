66a85b28a35b499bc7e9208cb2b4f18e57696a49
plitche <kysu728@naver.com>
2022-12-01 23:08:50 +0900

    setting base project

---
+++ Socket/zip/src/main/java/Socket/controller/ChatController.java
@@ -0,0 +1,35 @@
+package Socket.controller;
+
+import Socket.service.ChatService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.management.ServiceNotFoundException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/chat")
+public class ChatController {
+
+    final ChatService chatService;
+
+    @RequestMapping()
+    public String goChatPage(HttpServletRequest request,
+                             Model model) throws ServiceNotFoundException {
+        HttpSession httpSession = request.getSession();
+        String memberId = (String) httpSession.getAttribute("memberId");
+
+        if (memberId == null) {
+            throw new ServiceNotFoundException("Not login Exception - ChatController");
+        }
+
+        int applyCount = chatService.applyChat(memberId);
+        model.addAttribute("applyCount", applyCount);
+
+        return "/chat/main";
+    }
+}

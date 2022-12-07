881a0dfae669c84d5657820663785deffe8eac4a
plitche <kysu728@naver.com>
2022-12-07 23:08:50 +0900

    socket 진행 중

---
+++ Socket/zip/src/main/java/Socket/Sender.java
@@ -0,0 +1,38 @@
+package Socket;
+
+import java.io.DataOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Collections;
+import java.util.HashMap;
+
+public class MultichatServer {
+
+    HashMap<String,DataOutputStream> clients;
+
+    // 해쉬맵 생성, 여러개의 스레드가 동시에 접근 가능
+    //synchronizedMap(원래는 asynchronized)
+    MultichatServer() {
+        clients = new HashMap<>();
+        Collections.synchronizedMap(clients);
+    }
+
+    public void start() {
+        ServerSocket serverSocket = null;
+        Socket socket = null;
+        try {
+            //7777 port 열어놓기
+            serverSocket = new ServerSocket(7777);
+            System.out.println("server has started.");
+            while(true) {
+                socket = serverSocket.accept();
+                System.out.println("a new connection from [" + socket.getInetAddress() + ":" + socket.getPort() + "]");
+
+                //새로운 Client가 올때마다 스레드를 새로 생성한다.
+                ServerReceiver thread = new ServerReceiver(socket);
+                thread.start();
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
--- Socket/zip/src/main/java/Socket/controller/ChatController.java
+++ Socket/zip/src/main/java/Socket/controller/ChatController.java
@@ -1,6 +1,7 @@
 package Socket.controller;
 
 import Socket.service.ChatService;
+import ch.qos.logback.classic.net.server.ServerSocketReceiver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,6 +10,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import javax.management.ServiceNotFoundException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketAddress;
+import java.util.ArrayList;
 
 @Controller
 @RequiredArgsConstructor
@@ -19,7 +29,7 @@ public class ChatController {
 
     @RequestMapping()
     public String goChatPage(HttpServletRequest request,
-                             Model model) throws ServiceNotFoundException {
+                             Model model) throws ServiceNotFoundException, IOException {
         HttpSession httpSession = request.getSession();
         String memberId = (String) httpSession.getAttribute("memberId");
 
@@ -31,8 +41,41 @@ public class ChatController {
 
             model.addAttribute("isLogin", true);
             model.addAttribute("applyCount", applyCount);
+
+            // create client socket
+//            Socket socket = new Socket();
+//            SocketAddress address = new InetSocketAddress("yskwon", 9092);
+//            socket.connect(address);
+//
+//            //생성한 person 객체를 byte array로 변환
+//            byte[] bytes = toByteArray(applyCount);
+//            //서버로 내보내기 위한 출력 스트림 뚫음
+//            OutputStream os = socket.getOutputStream();
+//            //출력 스트림에 데이터 씀
+//            os.write(bytes);
+//            //보냄
+//            os.flush();
+
         }
 
         return "/chat/main";
+    }
+
+    public static byte[] toByteArray (Object obj)
+    {
+        byte[] bytes = null;
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        try {
+            ObjectOutputStream oos = new ObjectOutputStream(bos);
+            oos.writeObject(obj);
+            oos.flush();
+            oos.close();
+            bos.close();
+            bytes = bos.toByteArray();
+        }
+        catch (IOException ex) {
+            //TODO: Handle the exception
+        }
+        return bytes;
     }
 }

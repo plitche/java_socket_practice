efd40f48c5fbdae6ae90a0e52d77052caf285239
plitche <kysu728@naver.com>
2022-12-22 22:54:19 +0900

    chat_server_connect 소켓 서버 오류로 새로 생성

---
+++ Socket/SocketServer - 복사본/src/main/java/Socket/SocketApplication.java
@@ -0,0 +1,32 @@
+package Socket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+@SpringBootApplication
+public class SocketApplication {
+
+	public static void main(String[] args) throws IOException {
+		SpringApplication.run(SocketApplication.class, args);
+
+		ServerSocket serverSocket = new ServerSocket(9092);
+		Socket s = serverSocket.accept();
+
+		InputStreamReader in = new InputStreamReader(s.getInputStream());
+		BufferedReader bf = new BufferedReader(in);
+
+		String str = bf.readLine();
+//		int str2 = bf.read();
+		System.out.println("str = " + str);
+//		System.out.println("str2 = " + str2);
+//		serverSocket.close();
+	}
+
+}

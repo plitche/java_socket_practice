c1d0320480d9135b06dcc238a3a351f486d6d0f3
plitche <kysu728@naver.com>
2022-12-22 23:15:57 +0900

    create Socket server

--- Socket/SocketServer - 복사본/src/main/java/Socket/SocketApplication.java
+++ Socket/SocketServer - 복사본/src/main/java/Socket/SocketApplication.java
@@ -1,5 +1,6 @@
 package Socket;
 
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -13,20 +14,36 @@ import java.net.Socket;
 @SpringBootApplication
 public class SocketApplication {
 
+	private static final int PORT_NUMBER = 9092;
+
 	public static void main(String[] args) throws IOException {
 		SpringApplication.run(SocketApplication.class, args);
 
-		ServerSocket serverSocket = new ServerSocket(9092);
-		Socket s = serverSocket.accept();
+//		ServerSocket serverSocket = new ServerSocket(9092);
+//		Socket s = serverSocket.accept();
+//
+//		InputStreamReader in = new InputStreamReader(s.getInputStream());
+//		BufferedReader bf = new BufferedReader(in);
+//
+//		String str = bf.readLine();
+////		int str2 = bf.read();
+//		System.out.println("str = " + str);
+////		System.out.println("str2 = " + str2);
+////		serverSocket.close();
 
-		InputStreamReader in = new InputStreamReader(s.getInputStream());
-		BufferedReader bf = new BufferedReader(in);
+		System.out.println(":::                                                :::");
+		System.out.println(":::       Socket Application  Process Start        :::");
+		System.out.println(":::                                                :::");
 
-		String str = bf.readLine();
-//		int str2 = bf.read();
-		System.out.println("str = " + str);
-//		System.out.println("str2 = " + str2);
-//		serverSocket.close();
+		try(ServerSocket server = new ServerSocket(PORT_NUMBER)){
+			while(true){
+				Socket connection = server.accept();
+				Thread task = new SocketThreadServer(connection);
+				task.start();
+			}
+		}catch(IOException e){
+			e.printStackTrace();
+		}
 	}
 
 }

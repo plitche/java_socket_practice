7f75f00a82b9ad28292024d5e329880c734022de
plitche <kysu728@naver.com>
2022-12-28 23:42:52 +0900

    서버에서 보낸 메시지 클라이언트에서 받기

--- Socket/zip/src/main/java/Socket/controller/ChatController.java
+++ Socket/zip/src/main/java/Socket/controller/ChatController.java
@@ -18,6 +18,8 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketAddress;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
@@ -50,18 +52,42 @@ public class ChatController {
 
     @RequestMapping("/send")
     @ResponseBody
-    public void sendChat(@RequestParam String sendText) throws IOException {
-        // DEV용 코드 추후 삭제 필요 start
-        System.out.println("this.socket = " + this.socket);
-        if (this.socket == null) socket = new Socket("localhost", 9092);
-        // DEV용 코드 추후 삭제 필요 end
+    public Map<String, Object> sendChat(@RequestParam String sendText) throws IOException {
+        Map<String, Object> resultMap = new HashMap<>();
+        PrintWriter pw = null;
+        BufferedReader br = null;
 
-        PrintWriter pr = new PrintWriter(socket.getOutputStream());
-        pr.println(sendText);
-        pr.flush();
-        pr.close();
-//        this.socket.close();
-        this.socket = null;
+        try {
+            // 서버에 요청 보내기
+            if (this.socket == null) socket = new Socket("localhost", 9092);
+            System.out.println(socket.getInetAddress().getHostAddress() + "에 연결됨");
+            
+            // 메시지 전달
+            pw = new PrintWriter(socket.getOutputStream());
+            pw.println(sendText);
+            pw.flush();
+            pw.close();
+
+            // 메시지 받기
+            br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String returnMsg = br.readLine();
+            if (br.readLine() != null) resultMap.put("message", returnMsg);
+        } catch (IOException e) {
+            resultMap.put("message", "채팅 목록 조회 중 오류가 발생하였습니다.");
+            System.out.println(e.getMessage());
+        } finally {
+            // 소켓 닫기 (연결 끊기)
+            try {
+                if(socket != null) { socket.close(); }
+                if(br != null) { br.close(); }
+                if(pw != null) { pw.close(); }
+            } catch (IOException e) {
+                resultMap.put("message", "채팅 목록 조회 중 오류가 발생하였습니다.");
+                System.out.println(e.getMessage());
+            }
+        }
+
+        return resultMap;
     }
 
     public static byte[] toByteArray (Object obj)
